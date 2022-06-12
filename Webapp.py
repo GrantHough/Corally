b240bf6af789300e35b944210e0e9538ef2abb54
Grant Hough <grant.l.hough@gmail.com>
2022-06-11 18:30:41 -0700

    Created requirements.txt and started work on webapp

---
+++ Webapp.py
@@ -0,0 +1,9 @@
+from keras.models import load_model
+import streamlit as sl
+import numpy as np
+import cv2
+import glob
+import pandas as pd
+import tensorflow as tf
+
+sl.title("Welcome to Corally")
